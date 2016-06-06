decd18a70153a14d22b95b96127dfe4d8374a8cf
Alexis Quesney <alexis.quesney@ecota.co>
2016-06-06 15:59:09 +0200

    create boat model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606135555) do
+ActiveRecord::Schema.define(version: 20160606135826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boats", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
+    t.integer  "capacity"
+    t.string   "boat_picture"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "boats", ["user_id"], name: "index_boats_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -38,4 +50,5 @@ ActiveRecord::Schema.define(version: 20160606135555) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "boats", "users"
 end
