7bb4749932d44b4c74f09e84c1543da2f72bbeab
Alexis Quesney <alexis.quesney@ecota.co>
2016-06-06 16:06:19 +0200

    create availability model

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,5 @@
 class Boat < ActiveRecord::Base
   belongs_to :user
   has_many :bookings
+  has_many :availabilities
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606140302) do
+ActiveRecord::Schema.define(version: 20160606140524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "availabilities", force: :cascade do |t|
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "boat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "availabilities", ["boat_id"], name: "index_availabilities_on_boat_id", using: :btree
 
   create_table "boats", force: :cascade do |t|
     t.string   "name"
@@ -65,6 +75,7 @@ ActiveRecord::Schema.define(version: 20160606140302) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "availabilities", "boats"
   add_foreign_key "boats", "users"
   add_foreign_key "bookings", "boats"
   add_foreign_key "bookings", "users"
