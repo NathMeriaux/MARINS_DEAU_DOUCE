c33f5a5774b3db12efb5448897b8aea02ceae836
Alexis Quesney <alexis.quesney@ecota.co>
2016-06-06 17:11:35 +0200

    validates start/end dates in availability and booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,12 +2,14 @@ class Booking < ActiveRecord::Base
   belongs_to :user
   belongs_to :boat
   validates_presence_of :start_date, :end_date
-  validate :start_must_be_before_end_date
+  validate :start_date_before_end_date
 
-  private
+private
 
-  def start_must_be_before_end_date
-
+  def start_date_before_end_date
+    if self.start_date > self.end_date
+      errors.add(:start_date, "Start date should be before end date")
+    end
   end
 
 end
