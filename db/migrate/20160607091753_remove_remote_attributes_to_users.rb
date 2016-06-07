f6b69f7b5fe3e2a3bf36d9a6f49c3ef5cc261248
Marine Houze <m.houzedelaulnoit@gmail.com>
2016-06-07 15:37:05 +0200

    delete unuseful  migration removing remote_attributes

--- db/migrate/20160607091753_remove_remote_attributes_to_users.rb
+++
@@ -1,7 +0,0 @@
-class RemoveRemoteAttributesToUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :remote_first_name
-    remove_column :users, :remote_last_name
-    remove_column :users, :remote_picture
-  end
-end
