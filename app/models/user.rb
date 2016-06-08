9223a0706c049069d526ec75b3702302293ddb2d
Nath Meriaux <nath.meriaux@gmail.com>
2016-06-08 17:09:19 +0200

    upload photo ajouté dans model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: [:facebook]
 
+  mount_uploader :profile_picture, PhotoUploader
   has_many :boats, dependent: :destroy
   has_many :bookings
 
