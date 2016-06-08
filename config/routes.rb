b4f9663bb2a48d505effdd81707a51d493726aac
Marine Houze <m.houzedelaulnoit@gmail.com>
2016-06-08 17:57:47 +0200

    add user methods et lien navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:index, :edit, :update, :destroy]
+  root to: 'pages#home'
+
+  resources :users, only: [:show, :edit, :update, :destroy]
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
