84574885e1e5e966101da0ccb769bfabcb15b421
Marine Houze <m.houzedelaulnoit@gmail.com>
2016-06-09 17:30:35 +0200

    booking controller new create show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :edit, :update]
-  before_action :find_boat
+  before_action :find_boat, only: [:new, :create]
 
   def index
     @bookings = Booking.all
@@ -15,14 +15,33 @@ class BookingsController < ApplicationController
     @booking = Booking.new
   end
 
+  def check_create
+    (@booking.start_date.present?) &&
+    (not @booking.end_date.blank?) &&
+    (@booking.start_date < @booking.end_date)
+  end
+
   def create
     @booking = current_user.bookings.build(booking_params)
-    @booking.user_id = current_user.id if current_user
-    @booking.boat_id = @boat.id
-    if @booking.save
-      redirect_to boat_booking_path(@boat, @booking)
+    if check_create
+      check_availability(@boat, @booking.start_date, @booking.end_date)
+
+      if @availability != nil
+        @booking.boat_id = @boat.id
+
+        if @booking.save
+          update_availabilities(@availability, @booking.start_date, @booking.end_date)
+          @booking.update_attribute(:status, "pending")
+          flash[:notice] = "your reservation has been sent to the owner"
+          redirect_to boat_booking_path(@boat, @booking)
+        else
+          flash[:alert] = "booking not created!"
+        end
+      else
+        flash[:alert] = "This boat is not available at these dates"
+      end
     else
-      flash[:alert] = "booking not created!"
+        flash[:alert] = "Please enter start date < end date"
     end
   end
 
@@ -53,5 +72,21 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date, :status, :review_rating, :review_content)
+  end
+
+  def check_availability(boat, start_date, end_date)
+    boat.availabilities.each do |availability|
+
+      if start_date >= availability.start_date && end_date <= availability.end_date
+      @availability = availability
+    end
+    end
+    @availability
+  end
+
+  def update_availabilities(availability, start_date, end_date)
+    availability1 = @boat.availabilities.build(start_date: availability.start_date, end_date: start_date).save if start_date != availability.start_date
+    availability2 = @boat.availabilities.build(start_date: end_date, end_date: availability.end_date).save if end_date != availability.end_date
+    availability.destroy
   end
 end
